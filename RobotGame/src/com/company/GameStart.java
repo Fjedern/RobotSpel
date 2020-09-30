b30a1e530285a1609f0cc90688a0e431307ee2ef
Fjedern <38506398+Fjedern@users.noreply.github.com>
2020-09-30 20:00:58 +0200

    fix maybe

--- RobotGame/src/com/company/GameStart.java
+++ RobotGame/src/com/company/GameStart.java
@@ -81,66 +81,54 @@ public class GameStart {
                 }
 
                 animalList.get(j).setHitTheWall(false); // Reset boolean used in moveAnemalLeft etc. in Anemal clas, after the animal has done its random steps.
-                for (int d = 0; d < animalList.size(); d++) { // compare x and y value of each animals
-                    for (int a = d + 1; a < animalList.size(); a++) {
-                        if (animalList.get(d).getCurrentX() == animalList.get(a).getCurrentX() && animalList.get(d).getCurrentY() == animalList.get(a).getCurrentY()) {
-                            if (animalList.get(d).getName() != animalList.get(a).getName()) { // Checking collision between Zebra and Cheetah only not (zebra-zebra) or (cheetah-Cheetah)
+                for (int x = 0; x < animalList.size(); x++) { // compare x and y value of each animals  0
+                    for (int y = x + 1; y < animalList.size(); y++) {          //                         1
+                        if (animalList.get(x).getCurrentX() == animalList.get(y).getCurrentX() && animalList.get(x).getCurrentY() == animalList.get(y).getCurrentY() && animalList.get(x).getName() != animalList.get(y).getName()) {     //Om samma plats
+                            //if (animalList.get(x).getName() != animalList.get(y).getName()) { // Checking collision between Zebra and Cheetah only not (zebra-zebra) or (cheetah-Cheetah)
+                            if (animalList.get(x).getName() == 'Z') {   //bara om Z i ytterloop
+                                if (animalList.get(y).getHungry() == true) {
+                                    animalList.get(y).setHungry(false);
+                                    long time = currentTimeMillis();
+                                    animalList.get(y).setStartTime(time);
+                                    animalList.remove(x);
 
-                                Animal cheetah;
-                                Animal zebra;
-                                if (animalList.get(d).getName() == 'Z') {
-                                    zebra = animalList.get(d);
-                                    cheetah = animalList.get(a);
-                                    if (game.isCheetahHungry(cheetah) == true) {
-                                        animalList.remove(zebra);
-
-                                        for (int x = 0; x < animalList.size(); x++) { // compare x and y value of each animals  0
-                                            for (int y = x + 1; y < animalList.size(); y++) {          //                         1
-                                                if (animalList.get(x).getCurrentX() == animalList.get(y).getCurrentX() && animalList.get(x).getCurrentY() == animalList.get(y).getCurrentY() && animalList.get(x).getName() != animalList.get(y).getName()) {     //Om samma plats
-                                                    //if (animalList.get(x).getName() != animalList.get(y).getName()) { // Checking collision between Zebra and Cheetah only not (zebra-zebra) or (cheetah-Cheetah)
-                                                    if (animalList.get(x).getName() == 'Z') {   //bara om Z i ytterloop
-                                                        if (animalList.get(y).getHungry() == true) {
-                                                            animalList.get(y).setHungry(false);
-                                                            long time = currentTimeMillis();
-                                                            animalList.get(y).setStartTime(time);
-                                                            animalList.remove(x);
-
-                                                            zebraCount--;
-                                                            System.out.print("1");// test
-                                                        } else {
-                                                            long currentTime = currentTimeMillis() - animalList.get(y).getStartTime();
-                                                            //System.out.println(currentTime);
-                                                            if (currentTime > 10000) {
-                                                                animalList.get(y).setHungry(false);
-                                                                long time = currentTimeMillis();
-                                                                //System.out.println(time);
-                                                                animalList.get(y).setStartTime(time);
-                                                                animalList.remove(x);
-                                                                zebraCount--;
-                                                                System.out.print("2");
-                                                            }
-                                                        }
-                                                    }
-                                                    //}
-                                                }
-                                            }
-                                        }
-                                    }
-                                    game.printGameBoard();  //Print gameboard to console
-                                    System.out.printf("%s%n", "Zebror: " + zebraCount + " Cheetah: " + cheetahCount);   //Type out full cheetah count
-                                    Thread.sleep(50);   //Pause. Print-out speed
-
-                                    if (zebraCount == 0) {
-                                        gameRunning = false;
-                                        game.printGameBoard();  //Print gameboard to console
-                                        System.out.println("Game over all zebras are eaten!");
+                                    zebraCount--;
+                                    System.out.print("1");// test
+                                }
+                                else {
+                                    long currentTime = currentTimeMillis() - animalList.get(y).getStartTime();
+                                    //System.out.println(currentTime);
+                                    if (currentTime > 10000) {
+                                        animalList.get(y).setHungry(false);
+                                        long time = currentTimeMillis();
+                                        //System.out.println(time);
+                                        animalList.get(y).setStartTime(time);
+                                        animalList.remove(x);
+                                        zebraCount--;
+                                        System.out.print("2");
                                     }
                                 }
                             }
+                            //}
                         }
                     }
                 }
             }
+            game.printGameBoard();  //Print gameboard to console
+            System.out.printf("%s%n", "Zebror: " + zebraCount + " Cheetah: " + cheetahCount);   //Type out full cheetah count
+            Thread.sleep(50);   //Pause. Print-out speed
+
+            if (zebraCount == 0) {
+                gameRunning = false;
+                game.printGameBoard();  //Print gameboard to console
+                System.out.println("Game over all zebras are eaten!");
+            }
         }
     }
 }
+
+
+
+
+
+
